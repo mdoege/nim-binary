56b39e9503daa3a325960e7cb703cc95407fe6a5
Martin C. Doege <mdoege@compuserve.com>
2020-11-09 18:35:29 +0100

    add pure binary version

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 ## A binary clock in Nim using SDL2
 
+"binary.nim" uses vertical binary numbers to show time as six decimal digits (aka binary-coded decimal); "binary2.nim" is fully binary and shows horizontal binary numbers for hours, minutes and seconds.
+
 Compile with
 
-```nim c binary```
+```nim c binary``` and ```nim c binary2```
 
-Window size is fixed at 600x400 pixels.
+Window size is fixed at 600x400 pixels and 600x300 pixels, respectively.
+
+### BCD/decimal digit-based clock
+
+Each column of lights corresponds to a digit of decimal time.
 
 The time in the screenshot is 21:40:58.
 
 ![screenshot](https://github.com/mdoege/nim-binary/raw/master/screenshot.png "nim-binary screenshot")
+
+### Full binary clock
+
+The rows indicate hours, minutes and seconds in binary, with the most significant bit first.
+
+The time in the screenshot is 18:21:29.
+
+![screenshot](https://github.com/mdoege/nim-binary/raw/master/screenshot2.png "nim-binary2 screenshot")
--- binary2.nim
+++ binary2.nim
@@ -1,6 +1,6 @@
-## A binary clock in Nim using SDL2 (decimal digits/BCD)
+## A binary clock in Nim using SDL2 (fully binary)
 
-import sdl2, times, os, math
+import sdl2, times, os, strutils
 
 discard sdl2.init(INIT_EVERYTHING)
 
@@ -8,7 +8,7 @@ var
   window: WindowPtr
   render: RendererPtr
 
-window = createWindow("binary", 100, 100, 600, 400, SDL_WINDOW_SHOWN)
+window = createWindow("binary2", 100, 100, 600, 300, SDL_WINDOW_SHOWN)
 render = createRenderer(window, -1, Renderer_Accelerated or
     Renderer_PresentVsync or Renderer_TargetTexture)
 
@@ -20,10 +20,13 @@ var
 r.w = 80
 r.h = 80
 
-let rmax = [2, 4, 3, 4, 3, 4]
-
 while runGame:
-  let d = now().format("HHmmss")
+  let
+    d = now()
+    h = d.hour
+    m = d.minute
+    s = d.second
+    b = [toBin(h, 6), toBin(m, 6), toBin(s, 6)]
   while pollEvent(evt):
     if evt.kind == QuitEvent:
       runGame = false
@@ -31,16 +34,16 @@ while runGame:
   render.setDrawColor(0, 0, 0, 255)
   render.clear
 
-  for y in 0..3:
+  for y in 0..2:
     for x in 0..5:
-      if y >= rmax[x]:
+      if x == 0 and y == 0:
         continue
-      if (int(d[x]) - 48 and 2^y) > 0:
+      if int(b[y][x]) > 48:
         render.setDrawColor(255, 136, 0, 255)
       else:
         render.setDrawColor(0, 85, 170, 255)
       r.x = 10 + 100 * cint(x)
-      r.y = 310 - 100 * cint(y)
+      r.y = 10 + 100 * cint(y)
       render.fillRect(r)
 
   render.present
